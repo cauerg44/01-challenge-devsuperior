7c5e921ad3d79cfaa45aa7fcd0415b18df575319
Caue Garcia <cauerg7@outlook.com>
2023-11-18 16:23:47 -0300

    packs services and ents

---
+++ src/main/java/com/devsuperior/services/ShippingService.java
@@ -0,0 +1,13 @@
+package com.devsuperior.services;
+
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.ents.Order;
+
+@Service
+public class ShippingService {
+
+	public double shipment(Order order) {
+		
+	}
+}
